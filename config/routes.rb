ac2bb5f8a006af0a04f3ef051433aebd6896135a
Sebastian Strube <sebstrfra@gmail.com>
2021-05-21 11:56:54 +0200

    Basic CRUD working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # get "restaurants", to: "restaurants#index"
+
+  # get 'lists', to: 'lists#index'
+  # get 'lists/new', to: 'lists#new'
+  # post 'lists', to: 'lists#create'
+  # get 'lists/:id', to: 'lists#show', as: :list
+
+  # get 'lists/:id/bookmarks/new', to: 'bookmarks#new', as: :bookmarks
+  # post 'lists/:id/bookmarks', to: 'bookmarks#create'
+
+  root to: 'lists#index'
+
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: :destroy
+
 end
